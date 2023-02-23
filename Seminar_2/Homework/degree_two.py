ed5dbbc7b650420cadc42eb6d3913c8e6203a743
Alex <alexfike2016@gmail.com>
2023-02-23 17:57:43 +0500

    degree of two fix

--- Seminar_2/Homework/degree_two.py
+++ Seminar_2/Homework/degree_two.py
@@ -2,6 +2,6 @@ last_number = int(
     input('Введите число до которого нужно вывести все степени двойки: '))
 degree = 0
 
-while degree <= last_number//2:
+while degree*2 <= last_number:
     degree = 1 if degree == 0 else degree * 2
     print(degree, end=" ")
